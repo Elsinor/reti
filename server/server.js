94c861c967faa73a5e92bca48f45608d2726d4c5
Elsinor <kr3at00r@gmail.com>
2017-11-30 20:11:19 +0100

    added some log
    fix echoing the previous message

--- server/server.js
+++ server/server.js
@@ -2,27 +2,56 @@ const WebSocket = require('ws');
 
 /*
  * Bind Server on 8081 port
-*/
+ */
 const wss = new WebSocket.Server({ port: 8081 });
 
 /* 
  * onConnection Listener
-*/
+ */
 wss.on(
-    'connection',    
+    'connection',
     function connection(ws, req) {
         const ip = req.connection.remoteAddress;
-        console.log('New client: %s', ip);
+        ioLogger("New client.", ip, "Connection");
         ws.send('connected');
 
         /* 
          * onMessage Listener
-        */
+         */
         ws.on(
             'message',
             function incoming(message) {
-                console.log('received: %s', message);
+                ioLogger(message, ip, "Message");
                 ws.send('echoing: ' + message);
-            });        
+            });
     }
 );
+
+
+
+/* 
+ * Utils
+ */
+
+function addZero(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+
+function getDateString() {
+    var now = new Date();
+    var day = addZero(now.getDate());
+    var month = addZero(now.getMonth() + 1);
+    var year = now.getFullYear();
+    var hour = addZero(now.getHours());
+    var min = addZero(now.getMinutes());
+    var sec = addZero(now.getSeconds());
+
+    return day + "/" + month + "/" + year + " - " + hour + ":" + min + ":" + sec;
+}
+
+function ioLogger(msg, ip, desc) {
+    console.log('[' + getDateString() + ' :: ' + ip + ' :: ' + desc + ']: %s', msg);
+}
